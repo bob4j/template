cf0fe87da3c6c7bbc129546cf137ed694e26aa2e
Michael Bolgar <javarules01@gmail.com>
2016-04-03 11:04:49 +0300

    work

---
+++ src/main/java/com/openu/SpringService.java
@@ -0,0 +1,11 @@
+package com.openu;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SpringService {
+
+    public String getName() {
+        return "spring-service";
+    }
+}
