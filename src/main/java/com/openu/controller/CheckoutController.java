98650db706897dfd006f405c80308ec1bc7c5410
bob <javarules01@gmail.com>
2016-04-26 21:07:03 +0300

    started checkout page

---
+++ src/main/java/com/openu/controller/CheckoutController.java
@@ -0,0 +1,86 @@
+package com.openu.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.openu.model.Address;
+import com.openu.model.City;
+import com.openu.model.CreditCardInfo;
+import com.openu.model.CreditCardType;
+import com.openu.model.Customer;
+import com.openu.model.Order;
+import com.openu.model.OrderStatus;
+import com.openu.repository.CityRepository;
+import com.openu.repository.CustomerRepository;
+
+@Component
+@Scope("view")
+public class CheckoutController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private CreditCardInfo ccInfo;
+    private Address shippingAddress;
+    @Resource
+    private SessionBean sessionBean;
+    @Resource
+    private CustomerRepository customerRepository;
+    @Resource
+    private CityRepository cityRepository;
+
+    public CheckoutController() {
+        ccInfo = new CreditCardInfo();
+        shippingAddress = new Address();
+    }
+
+    @Transactional
+    public Double getTotalPrice() {
+        Customer customer = customerRepository.findOne(sessionBean.getCustomer().getId());
+        Order order = customer.getShoppingCart();
+        return order.getItems().stream().map(i -> i.getTotalPrice()).reduce(0D, (accumulator, i) -> accumulator + i);
+    }
+
+    @Transactional
+    public String order() {
+        Customer customer = customerRepository.findOne(sessionBean.getCustomer().getId());
+        Order order = customer.getShoppingCart();
+        order.setStatus(OrderStatus.PLACED);
+        order.setCcInfo(ccInfo);
+        order.setModified(System.currentTimeMillis());
+        order.setShippingAddress(shippingAddress);
+        customerRepository.save(customer);
+        return null;
+    }
+
+    public List<CreditCardType> getCcTypes() {
+        return Stream.of(CreditCardType.values()).collect(Collectors.toList());
+    }
+
+    public List<City> getCities() {
+        return (List<City>) cityRepository.findAll();
+    }
+
+    public CreditCardInfo getCcInfo() {
+        return ccInfo;
+    }
+
+    public void setCcInfo(CreditCardInfo ccInfo) {
+        this.ccInfo = ccInfo;
+    }
+
+    public Address getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(Address shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+}
